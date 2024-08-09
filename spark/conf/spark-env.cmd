ede782e1fe5be0587bf35f72fa206a4d3133adbc
borabii <benouirane.rabii@gmail.com>
2024-08-09 02:02:51 +0200

    airflow-spark worked

---
+++ spark/conf/spark-env.cmd
@@ -0,0 +1,3 @@
+set PATH=$PATH:/opt/bitnami/spark/hadoop/bin
+set HADOOP_CONF_DIR=/opt/bitnami/spark/conf/
+set SPARK_HADOOP_FS_DEFAULTFS=hdfs://namenode:8020
---
+++ spark/conf/spark-env.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/opt/bitnami/spark/hadoop/bin
+export HADOOP_CONF_DIR=/opt/bitnami/spark/conf
+export SPARK_HADOOP_FS_DEFAULTFS=hdfs://namenode:9000
